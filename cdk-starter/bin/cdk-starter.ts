5f2daaaa798f363e3f39be09712fce7cf7d0564b
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-01-20 06:38:33 +0530

    cdk aspects: add tags

--- cdk-starter/bin/cdk-starter.ts
+++ cdk-starter/bin/cdk-starter.ts
@@ -3,6 +3,7 @@ import * as cdk from "aws-cdk-lib";
 // import { CdkStarterStack } from "../lib/cdk-starter-stack";
 import { PhotosStack } from "../lib/PhotosStack";
 import { PhotosHandlerStack } from "../lib/PhotosHandlerStack";
+import { BucketTagger } from "./Tagger";
 
 const app = new cdk.App();
 // new CdkStarterStack(app, "CdkStarterStack");
@@ -12,3 +13,6 @@ const photosStack = new PhotosStack(app, "PhotosStack");
 new PhotosHandlerStack(app, "PhotosHandlerStack", {
   targetBucketArn: photosStack.photosBucketArn,
 });
+
+const tagger = new BucketTagger("level", "test");
+cdk.Aspects.of(app).add(tagger);
