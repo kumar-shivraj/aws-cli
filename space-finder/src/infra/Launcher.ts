90c820158ae1fd1263de18faebb068f360bfe292
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-01-20 08:09:54 +0530

    space-finder: implement api gateway using lambda

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -1,7 +1,12 @@
 import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
+import { ApiStack } from "./stacks/ApiStack";
 
 const app = new App();
 new DataStack(app, "DataStack");
-new LambdaStack(app, "LambdaStack");
+// new LambdaStack(app, "LambdaStack");
+const lambdaStack = new LambdaStack(app, "LambdaStack");
+new ApiStack(app, "ApiStack", {
+  helloLambdaInteration: lambdaStack.helloLambdaIntegration,
+});
--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -6,16 +6,20 @@ import {
   Runtime,
 } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 
 export class LambdaStack extends Stack {
+  public readonly helloLambdaIntegration: LambdaIntegration;
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new LambdaFunction(this, "HelloLambda", {
+    const helloLambda = new LambdaFunction(this, "HelloLambda", {
       runtime: Runtime.NODEJS_18_X,
       handler: "hello.main",
       //   code: Code.fromAsset(join(__dirname, "../../services")),
       code: Code.fromAsset(join(__dirname, "../..", "services")),
     });
+
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
 }
